a8043c63b676f4dd9d3a035530ff1be8f8ef0c0a
Mesnyankin Vladislav <goosts@mail.ru>
2022-02-17 20:08:11 +0300

    change font-family

--- src/js/home.js
+++ src/js/home.js
@@ -7,7 +7,7 @@ let unlock = true;
 
 const timeout = 800;
 
-if (popupLinks.length > 0){
+if (popupLinks.length > 0) {
     for (let index = 0; index < popupLinks.length; index++) {
         const popupLink = popupLinks[index];
         popupLink.addEventListener("click", function (e) {
@@ -19,45 +19,46 @@ if (popupLinks.length > 0){
     }
 }
 const popupCloseIcon = document.querySelectorAll('.close-popup');
-if(popupCloseIcon.length > 0){
-    for (let index = 0; index < popupCloseIcon.length; index++){
+if (popupCloseIcon.length > 0) {
+    for (let index = 0; index < popupCloseIcon.length; index++) {
         const el = popupCloseIcon[index];
-        el.addEventListener('click', function (e){
+        el.addEventListener('click', function (e) {
             popupClose(el.closest('.popup'));
             e.preventDefault();
         });
     }
 }
 
-function popupOpen(curentPopup){
-    if (curentPopup && unlock){
+function popupOpen(curentPopup) {
+    if (curentPopup && unlock) {
         const popupActive = document.querySelector('.popup.open');
         if (popupActive) {
             popupClose(popupActive, false);
-        }else {
+        } else {
             bodyLock();
         }
         curentPopup.classList.add('open');
-        curentPopup.addEventListener('click', function (e){
-            if(!e.target.closest('.popup_content')){
+        curentPopup.addEventListener('click', function (e) {
+            if (!e.target.closest('.popup_content')) {
                 popupClose(e.target.closest('.popup'));
             }
         });
     }
 }
-function popupClose(popupActive, doUnlock = true){
-    if (unlock){
+
+function popupClose(popupActive, doUnlock = true) {
+    if (unlock) {
         popupActive.classList.remove('open');
-        if (doUnlock){
+        if (doUnlock) {
             bodyUnlock();
         }
     }
 }
 
-function bodyLock(){
+function bodyLock() {
     const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
 
-    if(lockPadding.length > 0) {
+    if (lockPadding.length > 0) {
         for (let index = 0; index < lockPadding.length; index++) {
             const el = lockPadding[index];
             el.style.paddingRight = lockPaddingValue;
@@ -73,8 +74,8 @@ function bodyLock(){
 }
 
 
-function bodyUnlock(){
-    setTimeout(function (){
+function bodyUnlock() {
+    setTimeout(function () {
         if (lockPadding.length > 0) {
             for (let index = 0; index < lockPadding.length; index++) {
                 const el = lockPadding[index];
@@ -86,13 +87,13 @@ function bodyUnlock(){
     }, timeout);
 
     unlock = false;
-    setTimeout(function (){
+    setTimeout(function () {
         unlock = true;
     }, timeout);
 }
 
-document.addEventListener('keydown', function (e){
-    if(e.which === 27){
+document.addEventListener('keydown', function (e) {
+    if (e.which === 27) {
         const popupActive = document.querySelector('.popup.open');
         popupClose(popupActive);
     }
